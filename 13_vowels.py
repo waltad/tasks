10091646d26a221ac9f8567a445a796b400d5787
Tadek <tadek.waluga@gmail.com>
2021-11-01 19:54:34 +0100

    Changing in file 13_vowels.py

--- 13_vowels.py
+++ 13_vowels.py
@@ -6,4 +6,15 @@ Podpowiedź
 Warto zapisać poszukiwane samogłoski w postaci np. zbioru samogłosek."""
 
 
-def vowels(sentence):
+def vowels_number(sentence):
+    vowels = ['a', 'ą', 'A', 'Ą', 'e', 'ę', 'E', 'Ę', 'i', 'I', 'o', 'ó', 'O', 'Ó', 'u', 'U', 'y', 'Y']
+    counter = 0
+    for vowel in vowels:
+        counter += sentence.count(vowel)
+    print(f'There are {counter} vowels in this string.')
+
+
+if __name__ == '__main__':
+    text = 'Ważne: Spróbuj w swoim zadaniu wykorzystać Countera. Podpowiedź: Warto zapisać poszukiwane samogłoski w' \
+           ' postaci np. zbioru samogłosek.'
+    vowels_number(text)
